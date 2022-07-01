b4f08b616496282229c163b32ff5d1b4481978e0
D3konR3kon <dilavuyo@outlook.com>
2022-07-01 15:48:03 +0200

    4th commit

--- tapntake_frontend/src/app/app-routing.module.ts
+++ tapntake_frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { GetstartedComponent } from './components/getstarted/getstarted.component';
+
 const routes: Routes = [
   {
     path:'home',component:HomeComponent
+  },
+  {
+    path:'getstarted',component:GetstartedComponent
   }
 ];
 
