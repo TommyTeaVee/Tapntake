b475866632c2412a4b619c0797f5fe16d136c332
D3konR3kon <dilavuyo@outlook.com>
2022-07-04 16:01:59 +0200

    4th

--- tapntake_frontend/src/app/components/checkout/checkout.component.ts
+++ tapntake_frontend/src/app/components/checkout/checkout.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder,FormControl,FormControlName,FormGroup,Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-checkout',
+  selector: 'app-login',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required,Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+  ])
+  }); 
+  token:any
+  email: any
+  name: any
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+   
   }
+//   signIn() {
+//     let formData = JSON.parse(`${localStorage.getItem('users')}`);
+//     let exist = formData.length && 
+//     JSON.parse(`${localStorage.getItem('formData')}`).some((data: any) =>{data.email == this.email && data.password == this.password});
+//     if(!exist){
+//         alert("Incorrect login credentials");
+//     }
+//     else{
+//       this.router.navigate(['/home'])
+//     }
+   
+// }
 
+login(){
+  this.email = this.userForm.value.email;
+  this.name = this.userForm.value.name;
+  let users = JSON.parse(`${localStorage.getItem('users')}`)
+ 
+    users.find((elem:any ) => {
+      if((elem.name != this.name) || (elem.email != this.email))
+       return alert("Please provide correct credentials")
+      
+       else if(elem.password == this.name && elem.email == this.email)
+       localStorage.setItem('token', JSON.stringify(elem))
+        this.router.navigate(['home']) 
+       return 
+    })
+}
 }
