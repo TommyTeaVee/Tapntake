7832a2f1d9c47e782a87f01a345929dd809bb2c9
D3konR3kon <dilavuyo@outlook.com>
2022-07-04 15:17:05 +0200

    4th

--- tapntake_frontend/src/app/app-routing.module.ts
+++ tapntake_frontend/src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ShopmenuComponent } from './components/shopmenu/shopmenu.component';
+import {CartComponent} from './components/cart/cart.component'
+import { CheckoutComponent } from './components/checkout/checkout.component';
+// import {OrderComponent} from './components/order/order.component'
+// import { CheckoutComponent } from './components/checkout/checkout.component';
+
 
 
 const routes: Routes = [
@@ -10,6 +15,12 @@ const routes: Routes = [
   },
   {
     path:'shopmenu',component:ShopmenuComponent
+  },
+  {
+    path:'cart', component:CartComponent
+  },
+  {
+    path:'checkout', component: CheckoutComponent
   },
   
 ];
