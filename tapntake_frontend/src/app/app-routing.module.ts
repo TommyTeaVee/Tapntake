618e363b6ba17c03b3e5d97b28902d89b15a4011
D3konR3kon <dilavuyo@outlook.com>
2022-07-04 04:32:33 +0200

    menu

--- tapntake_frontend/src/app/app-routing.module.ts
+++ tapntake_frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ShopmenuComponent } from './components/shopmenu/shopmenu.component';
+
+
 const routes: Routes = [
   {
     path:'home',component:HomeComponent
-  }
+  },
+  {
+    path:'shopmenu',component:ShopmenuComponent
+  },
+  
 ];
 
 @NgModule({
